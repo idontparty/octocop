ccff55e97b2d78e5faac2b4159a6945ea0f642ce
idontparty <steffen.b.johansen@gmail.com>
2018-09-08 15:17:30 +1000

    install.sh added - install script for config files (dotfiles

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# Move black-hex.jpg to wallpapers folder
+if [ ! -f "~$USER/Pictures/Wallpapers" ]; then
+    mkdir "~USER/Pictures/Wallpapers"
+cp 'black-hex.jpg' "~USER/Pictures/Wallpapers"
+
+
+
